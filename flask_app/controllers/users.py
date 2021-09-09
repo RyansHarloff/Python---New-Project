a052ccce6d002fb1cbc9560ce3edd6262a5e8e8c
Ryan Harloff <ryans.harloff@gmail.com>
2021-09-08 20:04:59 -0500

    SecondCommit

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,8 +2,29 @@ from flask_app import app
 from flask import render_template,redirect,request, session
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
-from flask_app.models import User
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods = ["POST"])
+def register():
+    
+    if not User.validate_user(request.form):
+        return redirect("/")
+
+    hashed_pw = bcrypt.generate_password_hash(request.form['password'])
+    print(hashed_pw)
+
+    data = {
+        "first_name" : request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"],
+        "password" : hashed_pw,
+    }
+    User.create_user(data)
+    return redirect("/dashboard")
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -17,3 +17,28 @@ class User:
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    @staticmethod
+    def validate_user(data):
+        is_valid = True
+        if len(data['first_name']) < 2:
+            flash("First Name must be longer than 2 characters")
+            is_valid = False
+        if len(data["last_name"]) < 2:
+            flash("Last Name must be longer than 2 characters")
+            is_valid = False
+        if not EMAIL_REGEX.match(data["email"]):
+            flash("Not a valid email address")
+            is_valid = False
+        if len(data["password"]) < 8:
+            flash("Password must be at least 8 characters")
+            is_valid = False
+        if not data["password"] == data["conf_password"]:
+            flash("Password and Confirmation Password must Match")
+            is_valid = False
+        return is_valid
+
+    @classmethod
+    def create_user(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password, created_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW());"
+        return connectToMySQL("python-cars").query_db(query,data)
